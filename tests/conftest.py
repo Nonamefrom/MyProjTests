605840e971f5399a8ae3b5f515b051c849ccc79b
Alexandr Fomenko <sasha522000@gmail.com>
2023-08-02 22:52:07 +0300

    feat(init): Добавлен линтер, заморожены версии зависимостей

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,9 +7,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture(scope='function')
 def driver():
     options = webdriver.ChromeOptions()
-    # options.add_argument("--disable-dev-shm-usage")
-    # options.add_argument("--no-sandbox")
-    # options.add_argument("--headless")
     driver = webdriver.Remote(
         command_executor="http://chrome:4444/wd/hub",
         options=options
