e0736fa5db364a22b1a2576f69480fbf7934ac0e
IlinDenis <ilin.dv@svrauto.ru>
2023-08-09 00:22:43 +0300

    refaktor: вынес креды в новый config.ini, добавил тестовый класс в keycloack_auth_test.py  улучшающий отображение тестов в аллюр.

--- tests/keycloaktests/keycloack_auth_test.py
+++ tests/keycloaktests/keycloack_auth_test.py
@@ -1,51 +1,52 @@
 import allure
 import pytest
+import configparser
 from pages.base_page import BasePage
 from pages.keycloack_auth_page import KeycloackAuthForm
 from pages.controlpanelpages.topbar_cp_page import TopBarCpPage
 
-CP_URL = 'https://develop-cp.dev.svrauto.ru/auth/login'
-EMAIL = 'admin@svrauto.ru'
-WRONG_MAIL = 'admin@admin@ru23'
-WRONG_USER_PASS = '123456789'
-USER_PASS = 'adminPass'
-ERROR_TEXT = 'Неправильное имя пользователя или пароль.'
+config = configparser.ConfigParser()
+config.read('ini_config/config.ini')
+
+CP_URL = config.get('credentials', 'CP_URL')
+EMAIL = config.get('credentials', 'EMAIL')
+WRONG_MAIL = config.get('credentials', 'WRONG_MAIL')
+WRONG_USER_PASS = config.get('credentials', 'WRONG_USER_PASS')
+USER_PASS = config.get('credentials', 'USER_PASS')
+ERROR_TEXT = config.get('expected_results', 'ERROR_TEXT')
 
 
 @allure.suite("Тесты авторизации")
 @allure.sub_suite("Набор тестов Панели Управления")
+class TestLoginKeyCloack():
 # Авторизация существующего пользователя ПУ с неправильным паролем
-@allure.title("Авторизация сущест. пользователя с неправильным паролем")
-def test_login_wrong_pass(driver):
-    page = BasePage(driver, CP_URL)
-    page.open()
-    auth_form = KeycloackAuthForm(driver, url=CP_URL)
-    auth_form.login(EMAIL, WRONG_USER_PASS)
-    assert ERROR_TEXT == auth_form.error_message(), "Wrong error text"
+    @allure.title("Авторизация сущест. пользователя с неправильным паролем")
+    def test_login_wrong_pass(self, driver):
+        page = BasePage(driver, CP_URL)
+        page.open()
+        auth_form = KeycloackAuthForm(driver, url=CP_URL)
+        auth_form.login(EMAIL, WRONG_USER_PASS)
+        assert ERROR_TEXT == auth_form.error_message(), "Wrong error text"
 
 
 # Авторизация НЕсуществующего пользователя ПУ с правильным паролем
-@allure.title("Авторизация НЕсущест. пользователя с правильным паролем")
-def test_login_wrong_mail(driver):
-    page = BasePage(driver, CP_URL)
-    page.open()
-    auth_form = KeycloackAuthForm(driver, url=CP_URL)
-    auth_form.login(WRONG_MAIL, USER_PASS)
-    assert ERROR_TEXT == auth_form.error_message(), "Wrong error text"
+    @allure.title("Авторизация НЕсущест. пользователя с правильным паролем")
+    def test_login_wrong_mail(self, driver):
+        page = BasePage(driver, CP_URL)
+        page.open()
+        auth_form = KeycloackAuthForm(driver, url=CP_URL)
+        auth_form.login(WRONG_MAIL, USER_PASS)
+        assert ERROR_TEXT == auth_form.error_message(), "Wrong error text"
 
 
-# Авторизация существующего пользователя ПУ с правильным паролем
-@allure.title("Авторизация корректного пользователя")
-def test_login_correct_user(driver):
-    page = BasePage(driver, CP_URL)
-    page.open()
-    auth_form = KeycloackAuthForm(driver, url=CP_URL)
-    auth_form.login(EMAIL, USER_PASS)
-    assert 'Панель управления' == driver.title, "Wrong title of page, or wrong page was loaded"
-    top_bar = TopBarCpPage(driver, CP_URL)
-    top_bar.click_open_profile_dropdown().click_deauth_button()
-    assert auth_form.is_title_correct('Авторизация в internal'), "Wrong title after logout"
-
-
-if __name__ == "__main__":
-    pytest.main()
+# Авторизация/деавторизация существующего пользователя ПУ с правильным паролем
+    @allure.title("Авторизация корректного пользователя")
+    def test_login_correct_user(self, driver):
+        page = BasePage(driver, CP_URL)
+        page.open()
+        auth_form = KeycloackAuthForm(driver, url=CP_URL)
+        auth_form.login(EMAIL, USER_PASS)
+        assert 'Панель управления' == driver.title, "Wrong title of page, or wrong page was loaded"
+        top_bar = TopBarCpPage(driver, CP_URL)
+        top_bar.click_open_profile_dropdown().click_deauth_button()
+        assert auth_form.is_title_correct('Авторизация в internal'), "Wrong title after logout"
