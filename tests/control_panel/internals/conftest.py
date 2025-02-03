519b357eaeafd81c16e337df959a3a5abe674e12
DenisIlin <denni-dog@mail.ru>
2025-02-03 15:25:06 +0300

    Добавил в App ссылки и х импорты в фикстуру

--- tests/control_panel/internals/conftest.py
+++ tests/control_panel/internals/conftest.py
@@ -8,14 +8,17 @@ from steps.tables_steps import TablesSteps
 from pages.control_panel.ui.topbar_cp_page import TopBarCpPage
 from pages.control_panel.ui.sidebar_cp_page import SideBarCpPage
 from pages.keycloak.keycloack_auth_page import KeycloackAuthForm
+from pages.control_panel.options.all_options_page import AllOptionsCpPage
 # импорт панели навигации внутри опции
 from pages.control_panel.option_tab.fts_connection import FtsConnection
 from pages.control_panel.option_tab.pwz_admin_tab import AdminTab
 from pages.control_panel.option_tab.users import UsersTab
 from pages.control_panel.internal_user_page import InternalUserPage
+from pages.mailpit.mailpit_main import MailPitMain
 
 
 CP_URL = f"{Env().cp_url}/auth/login"
+MAIL_PIT_URL = f"{Env().mailpit}/"
 fixture = None
 
 
@@ -43,6 +46,8 @@ class Application:
         self.top_bar = TopBarCpPage(driver)
         self.side_bar = SideBarCpPage(driver)
         self.internal_page = InternalUserPage(driver)
+        self.mailpit_page = MailPitMain(driver, url=MAIL_PIT_URL)
+        self.cp_main = AllOptionsCpPage(driver, url=CP_URL)
 
         self.cp_auth_form = KeycloackAuthForm(driver, url=CP_URL)
 
